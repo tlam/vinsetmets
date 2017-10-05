9f0a581bd8af2a935d2e72a72e2d9f1b31d601ae
Thierry <lamthierry@gmail.com>
2017-10-04 21:04:01 -0400

    Adding grapes fixtures

--- lcbo/admin.py
+++ lcbo/admin.py
@@ -3,4 +3,25 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from lcbo.models import Booze
+
+
+class BoozeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'product_id',
+        'origin',
+        'price',
+        'inventory',
+        'primary_category',
+        'secondary_category',
+        'added_on',
+    )
+    list_filter = (
+        'primary_category',
+        'secondary_category',
+        'is_ocb',
+    )
+    search_fields = ['name']
+
+admin.site.register(Booze, BoozeAdmin)
